96e1d5050f2c629e4dd10542c9cd0e1008e6f8ae
Dev Zinzuvadiya <devzinzuvadiya1504@gmail.com>
2026-06-30 11:59:47 +0530

    manual engine controls

--- Class1.cs
+++ Class1.cs
@@ -20,6 +20,20 @@ public class Vehicle_Controls : Script
 
 
 
+    // --------------------------------------------------
+    // REQUIRED FIELDS FOR REALISTIC VEHICLE CONTROLS
+    // --------------------------------------------------
+
+    // <----- MANUAL ENGINE FIELD ----->
+    private Vehicle _currentVehicle = null;
+    private Vehicle _lastVehicle = null;
+    private bool _initializedVehicle = false;
+    private bool _currentEngineState = false;
+    private bool _vehicleExitKeyHeld = false;
+    private DateTime _vehicleExitKeyTime;
+
+
+
 
     public Vehicle_Controls()
     {
@@ -38,6 +52,19 @@ public class Vehicle_Controls : Script
     // =======================================================
     private void OnTick(object sender, EventArgs e)
     {
+        try
+        {
+            Ped player = Game.Player.Character;
+            if (player == null || !player.Exists() || player.IsDead) return;
+
+
+            if (player.IsInVehicle()) _currentVehicle = player.CurrentVehicle;
+            else _lastVehicle = player.LastVehicle;
+
+
+            PROCESS_VEHICLE_CONTROLS(player, _currentVehicle, _lastVehicle);
+        }
+        catch (Exception ex) { Notification.Show($"~r~OnTick~w~: {ex.Message}"); }
     }
 
 
@@ -46,6 +73,27 @@ public class Vehicle_Controls : Script
     // =======================================================
     private void OnKeyUp(object sender, KeyEventArgs e)
     {
+        try
+        {
+            Ped player = Game.Player.Character;
+            bool VehicleStatus = !player.IsInVehicle() || _currentVehicle == null || _currentVehicle.GetPedOnSeat(VehicleSeat.Driver) != player;
+
+            if (VehicleStatus && !player.IsInHeli || !player.IsInPlane) return;
+
+
+            if (e.KeyCode == _engineToggle)
+            {
+                if (!_currentEngineState)       // --- START ENGINE IF ENGINE CURRENTLY OFF --- //
+                {
+                    _currentEngineState = true;
+                    Function.Call(Hash.SET_VEHICLE_ENGINE_ON, _currentVehicle, true, false, true);
+                }
+                // --- TURN OFF ENGINE IF ENGINE CURRENTLY RUNNING --- //
+                else
+                    _currentEngineState = false; Function.Call(Hash.SET_VEHICLE_ENGINE_ON, false, false, true);
+            }
+        }
+        catch (Exception ex) { Notification.Show($"~r~OnKeyUp~w~: {ex.Message}"); }
     }
 
 
@@ -63,6 +111,43 @@ public class Vehicle_Controls : Script
     private void OnAbort(object sender, EventArgs e)
     {
     }
+
+
+
+
+
+    //  =>> WRAP ALL VEHICLE FEATURES INTO ONE MAIN FUNCTION  <<= //
+    // =======================================================
+    private void PROCESS_VEHICLE_CONTROLS(Ped player, Vehicle curVeh, Vehicle lasVeh)
+    {
+        VEHICLE_ENGINE_MANUAL_FEATURE(player, curVeh);
+    }
+
+
+    //  =>> MAKE SOME VEHICLE FEATURES FOR ACCESSABLE  <<= //
+    // =======================================================
+    private void VEHICLE_ENGINE_MANUAL_FEATURE(Ped player, Vehicle curVeh)
+    {
+        if (!player.IsInVehicle() || curVeh == null || curVeh.GetPedOnSeat(VehicleSeat.Driver) != player)
+        {
+            _initializedVehicle = false;
+            _currentEngineState = false; return;
+        }
+
+        // --- CHECK ENGINE STATE DURING ENTERING VEHICLE  --- //
+        if (!_initializedVehicle)
+        {
+            _initializedVehicle = true;
+
+            // CHECK CURRENT VEHICLE ENGINE STATE (in bool formate)
+            _currentEngineState = curVeh.IsEngineRunning;
+        }
+
+        // --- IF ENGINE STATE FOUND OFF, SO FORCE TO STAY OFF (avoid automatic) --- //
+        if (!_currentEngineState) Function.Call(Hash.SET_VEHICLE_ENGINE_ON, curVeh, false, false, true);
+    }
+
+
 
 
 
